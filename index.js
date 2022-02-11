7697f9e842fa0f0f7e002bf85d512ae5f5b280ca
wiktorklusek <wiktorklusek@yahoo.com>
2022-02-11 23:05:50 +0000

    exercises

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"));
+// #1
+//ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"));
+
+// #2
+//ReactDom.render(<h3>LONDON</h3>, document.getElementById("root"));
+
+// #3
+// Code using query selector
+//ReactDOM.render(<p>Hi, my name is Bob!</p>, document.querySelector("#root"))
+
+// #4
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
